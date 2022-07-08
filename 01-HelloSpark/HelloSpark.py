e729e4cb871dd1eb2df68ce74ebdfe8e86924310
KVR1988 <99358852+KVR1988@users.noreply.github.com>
2022-07-08 16:37:03 -0400

    ok

---
+++ 01-HelloSpark/HelloSpark.py
@@ -0,0 +1,33 @@
+import sys
+from pyspark.sql import *
+from lib.logger import Log4j
+from lib.utils import *
+
+if __name__ == "__main__":
+    conf = get_spark_app_config()
+
+    # spark = SparkSession \
+    #     .builder \
+    #     .appName("HelloSpark") \
+    #     .master("local[2]") \
+    #     .getOrCreate()
+    spark = SparkSession \
+            .builder \
+            .config(conf=conf) \
+            .getOrCreate()
+
+    logger = Log4j(spark)
+    logger.info("Arguments is {}".format(sys.argv[1]))
+    if len(sys.argv) != 2:
+        logger.error("Usage: HelloSpark <filename>")
+        sys.exit(-1)
+
+    logger.info("Starting HelloSpark")
+
+    survey_raw_df = load_survey_df(spark, sys.argv[1])
+    partitioned_survey_df = survey_raw_df.repartition(2)
+    count_df = count_by_country(partitioned_survey_df)
+    print(count_df.collect()[0]['Country'])
+
+    logger.info("Finished HelloSpark")
+    spark.stop()
